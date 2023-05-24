938d019d1c031d58d55448525e47d95506e5c905
Aarsyth <39145409+tobyarnett@users.noreply.github.com>
2023-05-24 12:09:10 -0500

    updated path folders

--- public/server/server.js
+++ public/server/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serving the static files from 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serving the static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port, () => {
