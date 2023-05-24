d7e048b1594e9f46b1863b86d3d1db4c32eb46d8
tobyarnett <39145409+tobyarnett@users.noreply.github.com>
2023-05-24 00:52:01 -0500

    Delete csvLoader.js

--- public/scripts/csvLoader.js
+++
@@ -1,9 +0,0 @@
-const loadCSV = require('./csvLoader');
-
-loadCSV('./path/to/your/file.csv')
-    .then(data => {
-        console.log(data); // logs the parsed CSV data
-    })
-    .catch(err => {
-        console.error(err); // logs any error that occurred while reading or parsing the file
-    });
