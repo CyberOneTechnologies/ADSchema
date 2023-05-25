5199bfb747320a6e060eb425f7d26028b5d4d319
Aarsyth <39145409+tobyarnett@users.noreply.github.com>
2023-05-24 23:33:26 -0500

    Updated file paths for folder structure

--- public/server/server.js
+++ public/server/server.js
@@ -6,10 +6,12 @@ const port = process.env.PORT || 3000;
 // Serving the static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Define the route for the root URL '/'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../', 'index.html'));
 });
 
+// Starting the server and listening on the specified port
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
